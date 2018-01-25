a2baca74161c3328d063fe44a133696ddda0c2d4
Chris Warner <cdw.lighting@gmail.com>
2018-01-25 16:18:12 -0500

    change static files dirs to include build

--- forge_macs/settings.py
+++ forge_macs/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "build")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
